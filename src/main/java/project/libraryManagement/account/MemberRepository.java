a62b07329b7ae1d317615b7eaaf18cdcfc1c01ac
JamogFrinds <ghrb6769@gmail.com>
2025-01-06 15:40:44 +0900

    feat: MemberService 기능 구현

--- src/main/java/project/libraryManagement/account/MemberRepository.java
+++ src/main/java/project/libraryManagement/account/MemberRepository.java
@@ -1,7 +1,17 @@
 package project.libraryManagement.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.libraryManagement.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByMemberId(String memberId);
+    List<Member> findByName(String name);
+
+    @Query("SELECT m FROM Member m WHERE m.name LIKE %:name%")
+    List<Member> searchByName(@Param("name") String name);
+
 }
--- src/main/java/project/libraryManagement/account/MemberService.java
+++ src/main/java/project/libraryManagement/account/MemberService.java
@@ -1,10 +1,50 @@
 package project.libraryManagement.account;
 
+import org.springframework.stereotype.Service;
 import project.libraryManagement.domain.Member;
 
-public interface MemberService {
-    Member login(String memberId, String memberPw);
-    void logout(String memberId);
-    Member signUp(Member member);
-    void destroyAccount(String memberId);
+import java.util.List;
+
+@Service
+public class MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member login(String memberId, String memberPw) {
+        Member member = memberRepository.findByMemberId(memberId);
+        if(member != null && member.getPassword().equals(memberPw)){
+            return member;
+        }
+        else{
+            throw new IllegalArgumentException("회원정보를 찾을 수 없습니다.");
+        }
+    }
+
+    public void logout(String memberId) {
+    }
+
+    public Long signUp(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public void destroyAccount(String memberId) {
+        Member member = memberRepository.findByMemberId(memberId);
+        if(memberId != null)
+            memberRepository.delete(member);
+        else
+            throw new IllegalArgumentException("회원정보를 찾을 수 없습니다.");
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
 }
