726e2220ce145b1685e92cef993dcda578610e44
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2025-02-12 16:39:33 +0900

    feat: 예약 도메인 생성자 추가 및 예약 서비스 생성

--- src/main/java/project/libraryManagement/domain/Reservation.java
+++ src/main/java/project/libraryManagement/domain/Reservation.java
@@ -2,13 +2,15 @@ package project.libraryManagement.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter
+@Getter @Setter
 public class Reservation {
     @Id @GeneratedValue
     @Column(name = "reservation_id")
@@ -27,10 +29,22 @@ public class Reservation {
     @OneToMany(mappedBy = "reservation")
     private final List<Rental> rentalList = new ArrayList<>();
 
-    private LocalDate reservationDate;
+    private LocalDateTime reservationDate;
 
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
 
-    protected Reservation() {}
+    public static Reservation createReservation(Member member, Book book, int count) {
+        Reservation reservation = new Reservation();
+        reservation.setMember(member);
+        reservation.setBook(book);
+        reservation.setBookQuantity(count);
+        reservation.setStatus(ReservationStatus.RESERVATION);
+        reservation.setReservationDate(LocalDateTime.now());
+        return reservation;
+    }
+
+    public void cancel() {
+        this.setStatus(ReservationStatus.CANCEL);
+    }
 }
---
+++ src/main/java/project/libraryManagement/service/ReservationService.java
@@ -0,0 +1,14 @@
+package project.libraryManagement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReservationService {
+    public void reservation(){}
+
+    public void cancel() {}
+}
